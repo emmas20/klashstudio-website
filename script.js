035db9bf7da3ac3e47795c69040d39f713c8c5b3
Emma <emma53294@gmail.com>
2026-05-29 12:02:45 -0600

    Fix menu click script

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector(".menu-toggle");
-  const navMenu = document.querySelector("nav");
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("#navMenu");
 
-  if (menuToggle && navMenu) {
-    menuToggle.addEventListener("click", function () {
-      navMenu.classList.toggle("active");
-    });
-  }
-});
+if (menuToggle && navMenu) {
+  menuToggle.onclick = function () {
+    navMenu.classList.toggle("active");
+  };
+}
